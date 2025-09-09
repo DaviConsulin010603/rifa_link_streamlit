84e92fd6b796d20786ab1155066f0f4d3e031f52
Davi Consulin <156239959+DaviConsulin010603@users.noreply.github.com>
2025-09-09 14:50:24 -0300

    Ajsute de texto

--- pages/erro_404.py
+++ pages/erro_404.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.set_page_config(page_title="404 — Página não encontrada", page_icon="❌", layout="centered")
 st.markdown("# 404 — Página não encontrada")
 st.write("")
-st.write("Como este é apenas um teste, a compra ainda não está disponível.")
-st.caption("Você pode fechar esta página agora. Obrigado!")
+st.write("COMPRA AINDA NÃO ESTÁ DISPONÍVEL.")
+st.caption("")
